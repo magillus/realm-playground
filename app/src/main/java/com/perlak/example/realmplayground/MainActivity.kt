f801193f3adad3c729cb0f8a03c61c297a57a7ca
Mateusz Perlak <Mat.Perlak@outcomehealth.com>
2017-10-11 15:42:12 -0500

    small update to listing repo changes

--- app/src/main/java/com/perlak/example/realmplayground/MainActivity.kt
+++ app/src/main/java/com/perlak/example/realmplayground/MainActivity.kt
@@ -2,7 +2,17 @@ package com.perlak.example.realmplayground
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.perlak.example.realmplayground.model.RepoGenerator
+import com.perlak.example.realmplayground.model.VcCommit
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +22,85 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnGenerate.setOnClickListener { v ->
+            Single.fromCallable {
+                var repoGenerator = RepoGenerator()
+                var repo = repoGenerator.generateData("test")
+                Realm.getDefaultInstance().use { realm ->
+                    realm.executeTransactionAsync { r ->
+                        r.deleteAll()
+                        r.insertOrUpdate(repo)
+                    }
+                }
+            }.subscribeOn(Schedulers.io()).subscribe()
+        }
+        commitList.layoutManager = LinearLayoutManager(this)
 
-            var repoGenerator = RepoGenerator()
-            var repo = repoGenerator.generateData("test")
+        commitList.adapter = adapter
 
+    }
+
+    var adapter = CommitAdapter()
+    var realm: Realm? = null
+
+    override fun onResume() {
+        super.onResume()
+        realm = Realm.getDefaultInstance()
+        realm?.let { r ->
+            var result = r.where(VcCommit::class.java).findAllSorted("dateTimeMillis")
+            result.addChangeListener { changeSet ->
+                adapter.setData(changeSet)
+            }
+            adapter.setData(result)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        realm?.close()
+    }
+}
+
+class CommitAdapter(var commitList: List<VcCommit> = emptyList()) : RecyclerView.Adapter<CommitViewHolder>() {
+    fun setData(newCommits: List<VcCommit>) {
+        commitList = newCommits
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: CommitViewHolder?, position: Int) {
+        holder?.bind(commitList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return commitList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CommitViewHolder {
+        return CommitViewHolder(parent!!)
+    }
+
+}
+
+class CommitViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.commit_view, parent, false)
+) {
+    var label = itemView.findViewById<TextView>(R.id.label)
+    var btnDelete = itemView.findViewById<Button>(R.id.btnDelete)
+
+    fun bind(commit: VcCommit) {
+        label.setText(commit.message)
+        btnDelete.tag = commit.id
+        btnDelete.setOnClickListener {
+            var commitId = btnDelete.tag as String
+            if (commitId != null) {
+                Single.fromCallable {
+                    // no need it if we use async transaction
+                    Realm.getDefaultInstance().use { realm ->
+                        realm.executeTransaction { r ->
+                            r.where(VcCommit::class.java).equalTo("id", commitId).findAll().deleteAllFromRealm()
+                        }
+                    }
+                }.subscribeOn(Schedulers.io()).subscribe()
+            }
         }
     }
 }
