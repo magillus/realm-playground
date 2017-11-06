77d925174a7ef83883cfe038ca41b57d39c088cd
Mateusz Perlak <mati.akip@gmail.com>
2017-11-06 07:15:09 -0600

    small update

--- app/src/main/java/com/perlak/example/realmplayground/model/Models.kt
+++ app/src/main/java/com/perlak/example/realmplayground/model/Models.kt
@@ -3,17 +3,24 @@ package com.perlak.example.realmplayground.model
 import io.realm.RealmList
 import io.realm.RealmModel
 import io.realm.RealmObject
+import io.realm.annotations.Ignore
 import io.realm.annotations.PrimaryKey
 import io.realm.annotations.RealmClass
 import java.util.*
 
 
-open class VcRepository(@PrimaryKey var id: String? = null, var url: String? = null, var name: String? = null) : RealmObject() {
-    var commits: RealmList<VcCommit> = RealmList()
-}
+open class VcRepository(
+        @PrimaryKey var id: String? = null,
+        var url: String? = null,
+        var name: String? = null,
+        var commits: RealmList<VcCommit> = RealmList()) : RealmObject()
 
 @RealmClass
-open class VcCommit(@PrimaryKey var id: String? = null, var message: String? = null, private var dateTimeMillis: Long = 0, var user: VcUser? = null) : RealmModel {
+open class VcCommit(
+        @PrimaryKey var id: String? = null,
+        var message: String? = null,
+        private var dateTimeMillis: Long = 0,
+        var user: VcUser? = null) : RealmModel {
 
     var dateTime: Date
         get() = Date(dateTimeMillis)
